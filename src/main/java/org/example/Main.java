bafc457be524be2cbf79fce4947d7c81f61dbae4
Piter Bilyk <bilykpiter492@gmail.com>
2024-11-18 21:34:34 +0200

    added Main.java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ComputationManager computationManager = new ComputationManager();
+        computationManager.start();
+    }
+}
